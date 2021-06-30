19d54bcedab13f3b97f5560702d7ec74624867ff
NgDuyPhong <NgDuyPhong@users.noreply.github.com>
2021-06-30 15:49:40 +0700

    Phong commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponentComponent } from './hello-component/hello-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'hello-component', component: HelloComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HelloComponentComponent } from './hello-component/hello-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponentComponent
   ],
   imports: [
     BrowserModule,
